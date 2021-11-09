d3df75bac60e12e85bec1b6536e1ddca0b239c37
Aurel AZON <aurel-azon@epitech.eu>
2021-11-09 16:42:41 -0500

    voila

--- exo3.c
+++ exo3.c
@@ -50,59 +50,77 @@ float *coef_vector(float *V, float m)
 }
 
 
-float *norm_vector(float * V)
+float norm_vector(float * V)
 {
-    float *norm = malloc(sizeof(float) * 3);
+    float norm = 0;
 
-    if (norm == NULL) {
-        return NULL;
-    } else {
-        norm = sqrt(pow(V[0], 2) + pow(V[1], 2) + pow(V[2], 2));
-    }
+    norm = sqrt(pow(V[0], 2) + pow(V[1], 2) + pow(V[2], 2));
     return (norm);
 }
+
+
+//declarer un vecteur V
+float cord_vector(float x, float y, float z, float xt, float yt, float zt, float n)
+{
+    while ( n != 0) {
+        xt = xt + x;
+        yt = yt + y;
+        zt = zt + z;
+        n--;
+    }
+    return (0);
+}
+
 /*
-int main(void)
-{
-    float *disp = abs_vector(2, 4, 6);
-    printf("x = %.3f\n", disp[0]);
-    printf("y = %.3f\n", disp[1]);
-    printf("z = %.3f\n", disp[2]);
-    return (0);
-}
+  int main(void)
+  {
+  float *disp = abs_vector(2, 4, 6);
+  printf("x = %.3f\n", disp[0]);
+  printf("y = %.3f\n", disp[1]);
+  printf("z = %.3f\n", disp[2]);
+  return (0);
+  }
 
 
-int main(int ac, char **av)
-{
-    float * V = abs_vector(atof(av[1]), atof(av[2]), atof(av[3]));
-    printf("%.2f\n%.2f\n%.2f\n", V[0], V[1], V[2]);
-    return (0);
-}
+  int main(int ac, char **av)
+  {
+  float * V = abs_vector(atof(av[1]), atof(av[2]), atof(av[3]));
+  printf("%.2f\n%.2f\n%.2f\n", V[0], V[1], V[2]);
+  return (0);
+  }
 
-int main (int ac, char **av)
-{
-    float * V1 = abs_vector(atof(av[1]), atof(av[2]), atof(av[3]));
-    float * V2 = abs_vector(atof(av[4]), atof(av[5]), atof(av[6]));
-    float *V = sum_vector(V1, V2);
-    printf("%.2f\n%.2f\n%.2f\n", V[0], V[1], V[2]); 
-    return (0);
-}
+  int main (int ac, char **av)
+  {
+  float * V1 = abs_vector(atof(av[1]), atof(av[2]), atof(av[3]));
+  float * V2 = abs_vector(atof(av[4]), atof(av[5]), atof(av[6]));
+  float *V = sum_vector(V1, V2);
+  printf("%.2f\n%.2f\n%.2f\n", V[0], V[1], V[2]); 
+  return (0);
+  }
 
 
-int main (int ac, char **av)
-{
-    float * V = abs_vector(atof(av[1]), atof(av[2]), atof(av[3]));
-    float * m = abs_vector(atof(av[0]));
-    float *Vx = coef_vector(V, m);
-    printf("%.2f\n%.2f\n%.2f\n", V[0], V[1], V[2]);
-    return (0);
-}
+  int main (int ac, char **av)
+  {
+  float * V = abs_vector(atof(av[1]), atof(av[2]), atof(av[3]));
+  float * m = abs_vector(atof(av[0]));
+  float *Vx = coef_vector(V, m);
+  printf("%.2f\n%.2f\n%.2f\n", V[0], V[1], V[2]);
+  return (0);
+  }
+
+  int main (int ac, char **av)
+  {
+  float * V = abs_vector(atof(av[1]), atof(av[2]), atof(av[3])); 
+  float k = norm_vector(V);
+  printf("%.2f\n", k);
+  return (0);
+  }
 */
 
-int main (int ac, char **av)
+int main ()
 {
-    float * V = abs_vector(atof(av[1]), atof(av[2]), atof(av[3])); 
-    float k = norm_vector(V);
-    printf("%.2f\n", k);
-    return (0);
+    printf("%.2f\n", n); 
+    float * V = abs_vector(atof(av[1]), atof(av[2]), atof(av[3]));
+    float k = cord_vector(V);
+    printf("%.2f\n%.2f\n%.2f\n", ); 
 }
